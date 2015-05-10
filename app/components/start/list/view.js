9755313e98eeface8f658a9d3eab733bc9e773a9
florentdancy <florent@dancy.io>
2015-05-10 23:47:27 +0200

    improvements on start page

--- app/components/start/list/view.js
+++ app/components/start/list/view.js
@@ -2,11 +2,21 @@ define(function(require, exports, module) {
   "use strict";
 
   var app = require("app");
+  var Play = require("components/play/index");
 
   var Layout = Backbone.Layout.extend({
     template: require("ldsh!./template"),
 
     initialize: function() {
+
+    },
+
+    events: {
+      "click .start": "startGame"
+    },
+
+    startGame: function(){
+
     }
   });
 
